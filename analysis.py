b56caa86c8cb994e9afe061ee5c47e0a69599e0c
Arnau Rodríguez Rubio <arnaurr94@hotmail.com>
2025-08-19 21:14:21 +0200

    Created functions to analyse the time spent on each step and account for errors. Updated notebook to implement said functions.

---
+++ analysis.py
@@ -0,0 +1,29 @@
+def time_diff(dataframe):
+    """Orders the DF values by 'visit_id' and 'date_time' (inplace). Then groups by 'visit_id' and applies
+    .diff() on column 'date_time'. Stores the values in a new column 'time_diff'.\n
+    Returns the updated dataframe"""
+
+    ## This will be useful for analysing the average duration users spend on each step.
+
+    dataframe.sort_values(by=['visit_id', 'date_time'], ignore_index=True, inplace=True)
+    dataframe['time_diff'] = dataframe.groupby('visit_id')['date_time'].diff()
+    return dataframe
+
+def step_diff(dataframe):
+    """Modifies the categorical column 'process_step' to int values. Then groups by 'visit_id' and applies
+    .diff() on column 'process_step'. Stores the values in a new column 'step_diff'.\n
+    Returns the updated dataframe"""
+
+    ## This will be useful for analysing if there's a step where users go back to a previous step, it may 
+    ## indicate confusion or an error.
+
+    dataframe['process_step'].replace({
+        'start':0,
+        'step_1':1,
+        'step_2':2,
+        'step_3':3,
+        'confirm':4
+    }, inplace=True)
+
+    dataframe['step_diff'] = dataframe.groupby('visit_id')['process_step'].diff()
+    return dataframe
