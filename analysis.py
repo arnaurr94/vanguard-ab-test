1ce11cc69f26ed694c8b65a9071fe4d29f85a3dc
Arnau Rodríguez Rubio <arnaurr94@hotmail.com>
2025-08-21 12:04:14 +0200

    Modified analysis.py to include a function to run queries through an engine.

--- analysis.py
+++ analysis.py
@@ -1,7 +1,13 @@
+import pandas as pd
+import getpass
+from sqlalchemy import create_engine, text
+
 def time_diff(dataframe):
-    """Orders the DF values by 'visit_id' and 'date_time' (inplace). Then groups by 'visit_id' and applies
+    """
+    Orders the DF values by 'visit_id' and 'date_time' (inplace). Then groups by 'visit_id' and applies
     .diff() on column 'date_time'. Stores the values in a new column 'time_diff'.\n
-    Returns the updated dataframe"""
+    Returns the updated dataframe
+    """
 
     ## This will be useful for analysing the average duration users spend on each step.
 
@@ -10,9 +16,11 @@ def time_diff(dataframe):
     return dataframe
 
 def step_diff(dataframe):
-    """Modifies the categorical column 'process_step' to int values. Then groups by 'visit_id' and applies
+    """
+    Modifies the categorical column 'process_step' to int values. Then groups by 'visit_id' and applies
     .diff() on column 'process_step'. Stores the values in new columns 'step_diff' and 'step_cum_sum'.\n
-    Returns the updated dataframe"""
+    Returns the updated dataframe
+    """
 
     ## This will be useful for analysing if there's a step where users go back to a previous step, it may 
     ## indicate confusion or an error.
@@ -28,3 +36,18 @@ def step_diff(dataframe):
     dataframe['step_diff'] = dataframe.groupby('visit_id')['process_step'].diff()
     dataframe['step_cum_sum'] = dataframe.groupby('visit_id')['step_diff'].cumsum()
     return dataframe
+
+def query_db(query):
+    """
+    Takes a mySQL query for the 'vanguard' DB (see ERD).\n
+    Returns the output table as a dataframe.
+    """
+    password = getpass.getpass()
+    db_name = "vanguard"    # DB used throughout the project
+
+    # Connect to vanguard DB
+    engine = create_engine(f'mysql+pymysql://root:{password}@localhost/{db_name}')
+
+    # Queries to extract info -- How many sessions are there per new or long standing client?
+    with engine.connect() as conn:
+        return pd.DataFrame(conn.execute(text(query)).fetchall())
